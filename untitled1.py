61b6fa4394879cc234e75eb7c846fea8872ef101
Jayantika2010 <79147549+jayantika51@users.noreply.github.com>
2022-12-22 21:16:42 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,32 @@
+from tkinter import*
+
+root=Tk()
+root.title("Ascii")
+
+root.geometry("500x500")
+root.configure(background='peach')
+
+
+enter_word = Entry(root)
+enter_word.plave(relx=0.5,rely=0.5,anchor=CENTER)
+#label
+label=Label(root,text="Name in ASCII :",bg="light yellow", fg="black")
+
+#function
+def asciiConverter():
+    
+    #input_box
+    input_word = enter_word.get()
+    
+    for letter in input_word :
+        label["text"] += str(ord(letter)) +"  "
+        
+        #button
+        btn=Button(root,text="Show name in Ascii", command=asciiConverter, bg='gold',fg='blue')
+        btn.place(relx=0.5, rely=0.5,anchor=CENTER)
+        
+        #LABEL
+        label.place(relx=0.5,rely=0.5,anchor=CENTER)
+         #End of funtion
+        
+root.mainloop()
